4b38d1e2ef6de2edefb75f7131a4b35dea0d3408
JoranMat <jorancoleman@gmail.com>
2023-09-14 12:42:48 -0500

    boleans and conditionals

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,12 +1,1 @@
-   // This demo shows off comments!
-
-   // console.log("This does not print.");
-
-   console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
-
-   /* Here is how
-   to have
-   multi-line
-   comments. */
-
-   console.log("Comments make your code more readable by others.");
+console.log(2**2**3*3)
